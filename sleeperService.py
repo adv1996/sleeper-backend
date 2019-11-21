3f64540fa24edc155cff574add617f20832a1c2f
Advaith Venkatakrishnan <advaith96@gmail.com>
2019-11-20 19:08:53 -0500

    updated week 11 data

--- sleeperService.py
+++ sleeperService.py
@@ -105,7 +105,7 @@ def serviceMultipleWeeks():
       generateOutput(str(w))
 
 serviceMultipleWeeks()
-# generateWeeklyScores()
+generateWeeklyScores()
 
 def combineSnapshotPyramids():
   league_id = sys.argv[1]
