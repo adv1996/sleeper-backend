b4726858908cf2cdd6744a2b11783900a6e3c221
Advaith Venkatakrishnan <advaith96@gmail.com>
2019-11-20 20:47:22 -0500

    removed deleted files

--- sleeperService.py
+++ sleeperService.py
@@ -104,7 +104,7 @@ def serviceMultipleWeeks():
       service(str(w))
       generateOutput(str(w))
 
-serviceMultipleWeeks()
+# serviceMultipleWeeks()
 # generateWeeklyScores()
 
 def combineSnapshotPyramids():
